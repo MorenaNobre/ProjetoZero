efb80de296e03b85b6086078b3e7d750436a9f98
MorenaNobre <morenagnobre@gmail.com>
2022-02-12 16:08:02 -0300

    Starting Project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,22 @@
-import Document from 'next/document';
+/* eslint-disable prettier/prettier */
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Poppins:wght@600&family=Roboto:ital,wght@0,400;0,700;1,900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
