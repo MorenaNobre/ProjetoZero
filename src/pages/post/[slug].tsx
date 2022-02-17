a386a1f92267375a00a7932b1be8795d2955d486
MorenaNobre <morenagnobre@gmail.com>
2022-02-17 19:31:11 -0300

    Structure and Style of post page

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,6 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -26,8 +28,55 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post({post}: PostProps) {
-  
+export default function Post({ post }: PostProps) {
+  return (
+    <>
+      <Header />
+      <img src="/Banner.png" alt="banner" className={styles.banner} />
+      <main className={commonStyles.container}>
+        <div className={styles.post}>
+          <div className={styles.postTop}>
+            <h1>Algum Título</h1>
+            <ul>
+              <li>
+                <FiCalendar />
+                17 Fev 2022
+              </li>
+              <li>
+                <FiUser />
+                Darth Varder
+              </li>
+              <li>
+                <FiClock />
+                15 min
+              </li>
+            </ul>
+          </div>
+
+          <article>
+            <h2>Título sessão</h2>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae
+              corporis quas fugit numquam,{' '}
+              <strong> totam minima recusandae quod </strong>provident id! Sed
+              alias provident ducimus nemo quis placeat architecto, hic non
+              assumenda a dolorem reprehenderit! Maiores ipsa cumque repellendus
+              facilis minima <a href=""> veniam repellat placeat mollitia </a>
+              fugiat quidem numquam consectetur dignissimos ab qui itaque
+              nesciunt assumenda necessitatibus quibusdam blanditiis, tempora
+              magnam autem. Eum, mollitia doloribus.{' '}
+              <pre>
+                {' '}
+                Reiciendis, distinctio error. Dolores incidunt ratione similique
+                nisi sed sapiente qui cum eum sunt<br/>ipsam consectetur, pariatur
+                alias.
+              </pre>
+            </p>
+          </article>
+        </div>
+      </main>
+    </>
+  );
 }
 
 // export const getStaticPaths = async () => {
